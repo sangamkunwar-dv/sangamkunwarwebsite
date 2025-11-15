524fe56993108a37dabf07ba8a56015fc0933f65
sangam kunwar <sangamkunwar48@gmail.com>
2025-11-15 20:17:27 +0545

    ugrade supabase

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,9 +3,9 @@
 import { createBrowserClient } from "@supabase/ssr"
 
 export function createClient() {
-  const url = "https://uhunxhnvvadynrfbsowp.supabase.co"
+  const url = "https://aydaylqulmokedvkemwo.supabase.co"
   const key =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVodW54aG52dmFkeW5yZmJzb3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MjQ4MzksImV4cCI6MjA3NjQwMDgzOX0._-PUTbuGWF43nTZYOotl3zCem9S_vLVJe2lg0mRtx1E"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5ZGF5bHF1bG1va2VkdmtlbXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyMTA3OTgsImV4cCI6MjA3ODc4Njc5OH0.45LTMHN4kBwqC6IVbWZgf7bzjDBsFru4hryZU0Y3jww"
 
   return createBrowserClient(url, key)
 }
