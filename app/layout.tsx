647d5d7c1cd032b7cf9384c192f5a90529949d4f
sangam kunwar <sangamkunwar48@gmail.com>
2025-12-06 13:27:41 +0545

    add image in title

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",         
     shortcut: "/favicon.ico",
-    apple: "https://sangamkunwar.netlify.app/sangamkunwarphoto.jpeg",
+    apple: "public\sangamkunwarphoto.jpeg",
   },
 }
 
